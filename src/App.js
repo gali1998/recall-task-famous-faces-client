9281415130a7e4b63e5f7711def4d14e014fe208
Gali Geller <gali1998@gmai.com>
2021-04-16 22:04:30 +0300

    30 minutes+ ugly consent

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Spreadsheet from './spreadsheet';
+import Consent from './Consent';
 import Identification from './Identification';
 import EndOfExperiment from './EndOfExperiment';
 import ReactTimeout from 'react-timeout'
@@ -11,12 +12,16 @@ import { getMinimizedGrid } from './gridGenerator';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {startTime: null, showId: true, showSpreadsheet: false, showEndOfExperiment: false, id: '', table: [], didGetTable: false, didSendToServer: false};
+    this.state = {didConsent: false, startTime: null, showId: false, showSpreadsheet: false, showEndOfExperiment: false, id: '', table: [], didGetTable: false, didSendToServer: false};
     
   }
 
+  consent(){
+      this.setState({didConsent: true, showId: true})
+  }
+
   handleClick(id){
-    let dur = 3600000
+    let dur = 3600000/ 2;
     this.setState({showId: false, showSpreadsheet: true, id: id, startTime: new Date()});
     this.props.setTimeout(this.endExperiment,  dur);
     // one hour is 3600000 milliseconds
@@ -54,6 +59,7 @@ class App extends Component {
    
     return (
       <div className="general">
+        <Consent clickAction = {this.consent.bind(this)} show = {this.state.didConsent == false} />
         <Identification clickAction = {this.handleClick.bind(this)} show = {this.state.showId}/>
         <Spreadsheet id={this.state.id} getData={this.getData.bind(this)} ended={this.state.showEndOfExperiment} didGetTable={this.state.didGetTable} show = {this.state.showSpreadsheet}/>
         <EndOfExperiment sendToServer={this.sendToServer.bind(this)} closeWindow={this.state.didSendToServer} id={this.state.id} show = {this.state.didGetTable} />
